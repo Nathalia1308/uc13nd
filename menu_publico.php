f888dc028ed4e7d9a39b2e5b30d11f8365d46039
Nathalia Barros da Silva <nathysilva.1502@gmail.com>
2022-11-24 11:52:21 -0300

    inicio index e menus

--- menu_publico.php
+++ menu_publico.php
@@ -1,5 +1,30 @@
-<?php 
+<?php ?>
 
+<!DOCTYPE html>
+<html lang="pt-BR">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu público</title>
+</head>
 
-?>
+<body>
+    <!-- abre a barra de navegação  -->
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <!-- Agrupamento mobile  -->
+            <div class="navbar-header">
+                <button type="button" class="nav-toggle collapsed" data-toggle="collapse" data-target="#menu-publico" aria-expanded="false">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="index.php" class="navbar navbar-brand"></a>
+            </div>
+        </div>
+    </nav>
+</body>
+
+</html>
