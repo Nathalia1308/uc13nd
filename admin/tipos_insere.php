77768e2b0fae4340767a52960993538298aba002
Nathalia Barros da Silva <nathysilva.1502@gmail.com>
2022-12-09 10:07:13 -0300

    Criação tipos insere e outros

--- admin/tipos_insere.php
+++ admin/tipos_insere.php
@@ -1,11 +1,26 @@
-<?php 
+<?php
 // Incluindo o sistema de autentificação
-include ("acesso_com.php");
+include("acesso_com.php");
 // incluir o arquivo e fazer a conexão
-include ("../conn/connect.php");
+include("../conn/connect.php");
 
+if ($_POST) {
+    # code...
+}
 $id_usuario = $_POST['id_usuario'];
 $login_usuario = $_POST['login_usuario'];
 $senha_usuario = $_POST['senha_usuario'];
+$nivel_usuario = $_POST['nivel_usuario'];
+
+// consulta sql para inserção de dados
+$insertSQL = "INSERT INTO tbprodutos (id_usuario, login_usuario, senha_usuario, nivel_usuario)
+              VALUES ('$id_usuario','$login_usuario','$senha_usuario','$nivel_usuario')";
+$resultado = $conn->query($insertSQL);
+//apos a ação a pagina sera redirecionada
+if (mysqli_insert_id($conn)) {
+    header("location: tipos_lista.php");
+} else {
+    header("location: tipos_lista.php");
+};
 
 ?>
